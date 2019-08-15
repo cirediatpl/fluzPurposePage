3d3b0eff615254d6a12292d898ad77aac616f25f
cirediatpl <cirediatpl@gmail.com>
2019-08-15 15:32:15 -0400

    added four team members

--- src/sections/Home/Team/Team.js
+++ src/sections/Home/Team/Team.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Team.scss";
 import DouglasBraun from "../../../assets/team/DouglasBraun.png";
+import Chase from "../../../assets/team/Chase-Fluz.jpg";
+import Gavin from "../../../assets/team/Gavin-Fluz.jpg";
+import Maurice from "../../../assets/team/Maurice-Fluz.jpg";
+import Rae from "../../../assets/team/Rae-Fluz.jpg";
 import TeamCard from "../../../components/TeamCard/TeamCard";
 
 const Team = () => {
@@ -16,13 +20,13 @@ const Team = () => {
           </p>
         </div>
         <div className="Team-content-cards columns">
-          <TeamCard img={DouglasBraun} name="Name" title="title" />
-          <TeamCard img={DouglasBraun} name="Name" title="title" />
-          <TeamCard img={DouglasBraun} name="Name" title="title" />
+          <TeamCard img={DouglasBraun} name="Douglas" title="title" />
+          <TeamCard img={Chase} name="Chase" title="title" />
+          <TeamCard img={Gavin} name="Gavin" title="title" />
         </div>
         <div className="Team-content-cards columns">
-          <TeamCard img={DouglasBraun} name="Name" title="title" />
-          <TeamCard img={DouglasBraun} name="Name" title="title" />
+          <TeamCard img={Maurice} name="Maurice" title="title" />
+          <TeamCard img={Rae} name="Rae" title="title" />
           <TeamCard img={DouglasBraun} name="Name" title="title" />
         </div>
         <div className="Team-content-cards columns">
